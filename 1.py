2206b19c33535aba61c30d8512a0959792423a9d
S <ssv273@mail.ru>
2019-01-10 03:53:55 +0700

    1

--- 1.py
+++
@@ -1,8 +0,0 @@
-num = str(input('Введите трехзначное число'))
-num_list = list(map(int, num))
-op = 1
-for i in num_list:
-    op = op * int(i)
-print('Произведение чисел = {}'.format(op))
-b = sum(num_list)
-print('Сумма чисел = {}'.format(b))
--- 6.py
+++
@@ -1,5 +0,0 @@
-import string
-
-num_char = int(input('Введите номер буквы в алфавите \n'))
-str_list = string.ascii_lowercase
-print('Это буква "{}"'.format(str_list[num_char - 1]))
